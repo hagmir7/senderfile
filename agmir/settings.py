e00eba44643ba727948f4423263c096d02610e91
Hassan Agmir <hagmir6@gmail.com>
2024-03-21 23:16:44 +0000

    Update static files

--- agmir/settings.py
+++ agmir/settings.py
@@ -120,7 +120,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))
+
+STATICFILES_DIRS = [
+    "/static/",
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
